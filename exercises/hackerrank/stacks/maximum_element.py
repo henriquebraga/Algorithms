325bb1702e41f0976bfdf36d03fa537e9a07adac
henrique.braga <henrique.braga@luizalabs.com>
2022-03-13 07:25:38 -0300

    exercise (stack): maximum element

---
+++ exercises/hackerrank/stacks/maximum_element.py
@@ -0,0 +1,72 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'getMax' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY operations as parameter.
+#
+
+def getMax(operations):
+    """
+       Idea is when whe push into stack, also push the max value at that point.
+       So, when we pop, if the stack gets empty, we reset the max. Otherwise, we peek the last max_value as the new max.
+        Performance is O(1), because just push/pop
+    """
+    stack = []
+    max_value = -1
+    max_values = []
+
+    for operation in operations:
+        splitted_op = operation.split(' ')
+        el = None
+        op = None
+        
+        if len(splitted_op) == 2:
+            op, el = splitted_op[0], splitted_op[1]
+        else:
+            op = splitted_op[0]
+        
+        if op == '1':
+            if int(el) > max_value: #if element is bigger, reset the max
+                max_value = int(el)
+            stack.append({'max': max_value, 'element': el})
+        if op == '2':
+            if len(stack) > 0:
+                stack.pop()
+            
+            if len(stack) == 0: #if stack is empty, reset max_value
+                max_value = -1
+            else:
+                max_value = stack[-1]['max'] #otherwise, we reset max_value to the new top element.
+    
+        if op == '3': #just append max from top of the stack
+            max_values.append(stack[-1]['max'])
+            
+    return max_values
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ops = []
+
+    for _ in range(n):
+        ops_item = input()
+        ops.append(ops_item)
+
+    res = getMax(ops)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
