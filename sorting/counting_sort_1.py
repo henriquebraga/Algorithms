5c576b63e17ad8998aecc541305fb3e23740130c
Henrique Braga <henrique.braga@henrique.braga>
2022-04-14 19:54:12 -0300

    exercise (sorting): counting sort 1

---
+++ sorting/counting_sort_1.py
@@ -0,0 +1,42 @@
+'''
+https://www.hackerrank.com/challenges/countingsort1/problem
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    counter = [0] * 100
+    
+    for i in arr:
+        counter[i] += 1
+    
+    return counter
+        
+        
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
