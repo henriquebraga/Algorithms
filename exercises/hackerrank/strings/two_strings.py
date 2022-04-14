7846a73d1c2b850cfc66af29e4380f959244a4dc
Henrique Braga <henrique.braga@henrique.braga>
2022-04-14 20:28:29 -0300

    exercise (string): two strings

---
+++ exercises/hackerrank/strings/two_strings.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'twoStrings' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+'''
+Problem: https://www.hackerrank.com/challenges/two-strings/
+'''
+
+def twoStrings(s1, s2):
+    return 'YES' if set(s1) & set(s2) else 'NO'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
