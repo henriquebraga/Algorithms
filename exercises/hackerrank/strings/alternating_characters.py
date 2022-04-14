c102215f5effacc71f7bc07897c920a0b2ba0fba
Henrique Braga <henrique.braga@henrique.braga>
2022-04-14 20:27:36 -0300

    exercise (string): alternating characters

---
+++ exercises/hackerrank/strings/alternating_characters.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    char = s[0]
+    delete_count = 0
+
+    for position, char in enumerate(s[1:]):
+        if char == s[position]:
+            delete_count += 1
+        else:
+            char = s[position]
+
+    return delete_count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
